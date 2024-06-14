29753155f40ad7205412565cc8500d9621051445
pomogranet <elliotgould5721@gmail.com>
2024-06-14 10:49:30 +0530

    Cleaned admins Includes folder

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<?php include "includes/header.php" ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+       <?php include "includes/navigation.php" ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+
+                        <h1 class="page-header">
+                            Welcome To Admin
+                            <small>User</small>
+                        </h1>
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+<?php include "includes/footer.php" ?>
---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+$db['db_host'] = 'localhost:4306';
+$db['db_user'] = 'root';
+$db['db_pass'] = '';
+$db['db_name'] = 'cms';
+
+foreach($db as $key => $value) {
+    define(strtoupper($key), $value);
+}
+
+
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if($connection) {
+    echo 'We Are Connected';
+}
+
+
+
+?>
