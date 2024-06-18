cad5a1b7633ca8d8d6a104bbc49a11a9e5b6cf50
pomogranet <elliotgould5721@gmail.com>
2024-06-18 17:31:27 +0530

    comment system count 27

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -156,7 +156,14 @@ if (isset($_POST['checkBoxArray'])) {
                 echo "<td>$post_status</td>";
                 echo "<td><img width='100' src='../img/$post_image' alt='image'></td>";
                 echo "<td>$post_tags</td>";
-                echo "<td>$post_comment_count</td>";
+
+$query = "SELECT * FROM comments WHERE comment_post_id = $post_id";
+$send_comment_query = mysqli_query($connection, $query);
+$count_comments = mysqli_num_rows($send_comment_query);
+                echo "<td><a href=''>$count_comments<a></td>";
+
+
+
                 echo "<td> $post_date </td>";
                 echo "<td> <a href='../post.php?p_id={$post_id}' > View Post </a> </td>";
                 echo "<td> <a href='posts.php?source=edit_post&p_id={$post_id}' > Edit </a> </td>";
--- post.php
+++ post.php
@@ -60,9 +60,6 @@
 
             header("Location: index.php");
         }
-            
-            
-            
             ?>
 
 
@@ -97,9 +94,9 @@
                 }
 
                 
-                $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-                $query .= "WHERE post_id = $the_post_id ";
-                $update_comment_count = mysqli_query($connection, $query);
+                // $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+                // $query .= "WHERE post_id = $the_post_id ";
+                // $update_comment_count = mysqli_query($connection, $query);
 
                 } else {
 
