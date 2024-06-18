37a52f556d29bb13b4db7d7d98b965e132ae0803
pomogranet <elliotgould5721@gmail.com>
2024-06-18 19:49:18 +0530

    securing URL from injections

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -117,17 +117,23 @@ if (isset($_GET['change_to_sub'])) {
 
 
 
-
+//support@edwindiaz.com
 
 if (isset($_GET['delete'])) {
 
-    $the_user_id = $_GET['delete'];
+    if(isset($_SESSION['user_role'])) {
+        if($_SESSION['user_role'] == 'admin') {
+
+     
+    $the_user_id =mysqli_real_escape_string($connection, $_GET['delete']) ;
 
     $query = "DELETE FROM users WHERE user_id = {$the_user_id} ";
     $delete_user_query = mysqli_query($connection, $query);
-
     confirm($delete_user_query);
 
 
     header("Location: users.php");
+
+}
+}
 }
